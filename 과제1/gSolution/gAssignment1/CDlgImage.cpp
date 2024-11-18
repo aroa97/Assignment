70bb6a744f520594a3fc8318fe40cfb27678e785
aroa97 <ehild97@gmail.com>
2024-11-18 17:26:24 +0900

    2024.11.18 업데이트

---
+++ 과제1/gSolution/gAssignment1/CDlgImage.cpp
@@ -0,0 +1,78 @@
+﻿// CDlgImage.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "gAssignment1.h"
+#include "afxdialogex.h"
+#include "CDlgImage.h"
+
+
+// CDlgImage 대화 상자
+
+IMPLEMENT_DYNAMIC(CDlgImage, CDialogEx)
+
+CDlgImage::CDlgImage(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CDIGIMAGE, pParent)
+{
+	m_pParent = pParent;
+}
+
+CDlgImage::~CDlgImage()
+{
+}
+
+void CDlgImage::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgImage, CDialogEx)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+// CDlgImage 메시지 처리기
+
+
+BOOL CDlgImage::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	InitImage();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void CDlgImage::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	// 그리기 메시지에 대해서는 CDialogEx::OnPaint()을(를) 호출하지 마십시오.
+
+	if (m_Image) {
+		m_Image.Draw(dc, 0, 0);
+	}
+}
+
+void CDlgImage::InitImage() 
+{
+	int nWidth = 640;
+	int nHeight = 480;
+	int nBpp = 8;
+
+	m_Image.Create(nWidth, -nHeight, nBpp);
+	if (nBpp == 8) {
+		static RGBQUAD rgb[256];
+		for (int i = 0; i < 256; i++) {
+			rgb[i].rgbRed = rgb[i].rgbGreen = rgb[i].rgbBlue = i;
+		}
+		m_Image.SetColorTable(0, 256, rgb);
+	}
+	int nPitch = m_Image.GetPitch();
+	unsigned char* fm = (unsigned char*)m_Image.GetBits();
+
+	memset(fm, 0xff, nWidth * nHeight);
+}
---
+++ 과제1/gSolution/gAssignment1/gAssignment1Dlg.h
@@ -0,0 +1,43 @@
+﻿
+// gAssignment1Dlg.h: 헤더 파일
+//
+#include "CDlgImage.h"
+
+#pragma once
+
+
+// CgAssignment1Dlg 대화 상자
+class CgAssignment1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CgAssignment1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	CDlgImage* m_pDlgImage;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GASSIGNMENT1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP();
+public:
+	int m_nX1;
+	int m_nY1;
+	int m_nX2;
+	int m_nY2;
+	afx_msg void OnDestroy();
+};
